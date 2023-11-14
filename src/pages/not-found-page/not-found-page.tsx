af3ec898fe094eb921500e6df425b8c52fc4f139
VitaliySotskov <vit6150@gmail.com>
2023-11-14 23:04:40 +0300

    Исправление ошибок

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,17 +1,17 @@
 import { Link } from 'react-router-dom';
 import {Helmet} from 'react-helmet-async';
-//import styles from './not-found-page.module.css';
+import '../../pages/not-found-page/not-faund-page.css';
 import { AppRoute } from '../../common/const';
 
 function NotFoundPage (): JSX.Element {
   return (
-    <section className="page" style={{height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+    <section className='NotFoundPage'>
       <Helmet>
         <title>six-cities - 404</title>
       </Helmet>
-      <h1 style={{marginTop: '100px', marginBottom: '50px', textAlign: 'center', fontSize: 50}}>404</h1>
-      <p style={{textAlign: 'center', fontSize: 32}}> Back To {' '}
-        <Link to={AppRoute.Main} style={{color:'#4481c3'}}> main </Link>
+      <h1 className='title'>404</h1>
+      <p className='text'> Back To {' '}
+        <Link to={AppRoute.Main} className='link'> Main Page </Link>
       </p>
     </section>
   );
